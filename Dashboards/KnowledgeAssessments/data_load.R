b4d643b1526961282b11449f114bcda55b09996b
dungates <duncan.gates123@gmail.com>
2021-11-10 16:11:50 -0800

    add script

--- Dashboards/KnowledgeAssessments/data_load.R
+++ Dashboards/KnowledgeAssessments/data_load.R
@@ -1,3 +1,5 @@
+################################ Data Loading Script for Knowledge Assessments Dashboard ################################
+
 ### ELA General Bootcamp ###
 save_processed_data(data = here::here("Dashboards/KnowledgeAssessments/data/unprocessed/ELABootcamp-General.rds"),
                     q_and_a = here::here("Dashboards/KnowledgeAssessments/data/questions_and_answers/ela_general_bootcamp.rds"),
@@ -70,42 +72,59 @@ save_processed_data(data = here::here("Dashboards/KnowledgeAssessments/data/unpr
                                 "An appropriate scaffold for language development is to allow students to make charts, diagrams, and other visual representations of their understanding."),
                     save_name = "math_bootcamp_eic")
 
+### Math: Bootcamp ###
+save_processed_data(data = here::here("Dashboards/KnowledgeAssessments/data/unprocessed/MathBootcamp.rds"),
+                    q_and_a = here::here("Dashboards/KnowledgeAssessments/data/questions_and_answers/math_bootcamp.rds"),
+                    correct = c("Going deeper into fewer math topics.",
+                                "Making connections between math topics across grades.",
+                                "Unguided problem solving lessons are the least effective type of math lesson.",
+                                "Building deep understanding with fewer math topics is more effective than covering a broader range of math topics.",
+                                "Creating opportunities for students to practice saying out loud how they solved a problem.",
+                                "Students do not need to understand English completely before they can start making sense of math instruction in English.",
+                                "An appropriate scaffold for language development is to allow students to make charts, diagrams, and other visual representations of their understanding."),
+                    save_name = "math_bootcamp")
 
+### Math Cycle of Inquiry I - Eliciting Student Thinking ###
+save_processed_data(data = here::here("Dashboards/KnowledgeAssessments/data/unprocessed/MathCycleofInquiryI-ElicitingStudentThinking.rds"),
+                    q_and_a = here::here("Dashboards/KnowledgeAssessments/data/questions_and_answers/math_cycle_inquiry_1.rds"),
+                    correct = c("How do we know that y=3x+2 represents a linear relationship?",
+                                "What key features of this graph tells us that the line represents a proportional relationship?",
+                                "How did Tessa think about this word problem?",
+                                "What connections do you see between Tessa and Fede’s strategies?",
+                                "The next day, a teacher leads a discussion analyzing a few different students’ solutions to the previous day’s problem set and connecting solutions to a learning goal.",
+                                "A plan for how to students should solve each step of a task",
+                                "Intentionally asking focusing questions of students who may have been historically pushed out of math success in the past",
+                                "Analyzing video or audio recordings of one’s own instruction regularly for bias",
+                                "Sharing frustrations"),
+                    save_name = "math_cycle_of_inquiry_i")
 
+### Math: Cycle of Inquiry V- Sequencing and Connecting Representations ###
+save_processed_data(data = here::here("Dashboards/KnowledgeAssessments/data/unprocessed/MathCycleofInquiryV-SequencingandConnectingRepresentations.rds"),
+                    q_and_a = here::here("Dashboards/KnowledgeAssessments/data/questions_and_answers/math_cycle_inquiry_5.rds"),
+                    correct = c("Anticipate, Monitor, Select, Sequence, Connect",
+                                "Emphasize the importance of planning to create an engaging mathematical discussion",
+                                "We lead students to a pre-planned mathematical idea during Connecting.",
+                                "We use student data to inform our planning during Anticipation.",
+                                "We can gather a variety of student work during Selecting.",
+                                "Presents tasks that offer multiple entry points",
+                                "Structures collaboration to use varying math knowledge and skills to solve complex problems",
+                                "How do I identify and support mathematical contributions from students with different strengths and levels of confidence?"),
+                    save_name = "math_cycle_inquiry_iv")
 
+### School Leaders: ELA ###
+save_processed_data(data = here::here("Dashboards/KnowledgeAssessments/data/unprocessed/SchoolLeadersELA.rds"),
+                    q_and_a = here::here("Dashboards/KnowledgeAssessments/data/questions_and_answers/ela_school_leaders.rds"),
+                    correct = c("Regular practice with complex texts and their academic language.",
+                                "Building knowledge through content-rich non-fiction.",
+                                "What can you infer from Dr. King’s letter about the letter that he received?",
+                                "In “The Lion, the Witch, and the Wardrobe”, how and why does Edmund change?",
+                                "It focuses on observations aligned to the ELA and literacy instructional shifts.",
+                                "It is a coaching tool that supports identifying equitable literacy practices.",
+                                "The feedback is focused on teacher moves such as the questions the teachers posed as students read the text.",
+                                "The interaction includes role-playing for the teacher to practice a new strategy which directly addresses a challenge from the observation."),
+                    save_name = "ela_school_leaders")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+################################################################################################################################
 
 
 
