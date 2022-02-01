44d6535ca4833e8e09b20ef1f43ccc638e53e227
dungates <duncan.gates123@gmail.com>
2022-02-01 05:54:41 -0800

    reset all

--- data-raw/survey_monkey.R
+++
@@ -1,5 +0,0 @@
-## code to prepare `survey_monkey` dataset goes here
-
-survey_monkey <- TeachingLab::survey_monkey
-survey_monkey %>% readr::write_csv("data-raw/survey_monkey.csv")
-usethis::use_data(survey_monkey, overwrite = TRUE, internal = F)
